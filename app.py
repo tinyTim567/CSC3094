8599a6a2d0eae23ed6d02a9827a3d90c3b8e0d2f
Thomas McKnight <tmck911@gmail.com>
2024-04-07 03:38:40 +0100

    Add basic templates for main site pages

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return render_template("main/index.html")
+
+
+@app.route("/phishing")
+def phishing():
+    return render_template("phishing/phishing.html")
+
+
+@app.route("/malware")
+def malware():
+    return render_template("malware/malware.html")
+
+
+@app.route("/passwords")
+def passwords():
+    return render_template("passwords/passwords.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("main/about.html")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("error/404.html"), 404
+
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template("error/500.html"), 500
 
 
 if __name__ == '__main__':
